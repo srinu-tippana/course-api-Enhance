f0bf4038b7516e344f0f0623d0f3deafecb657a5
srinu-tippana <svas7246@gmail.com>
2022-11-06 00:06:50 +0530

    course-api-Enhance

---
+++ src/main/java/com/enhance/springbootquickstart/courseapiEnhance/CourseApiEnhanceApplication.java
@@ -0,0 +1,13 @@
+package com.enhance.springbootquickstart.courseapiEnhance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiEnhanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiEnhanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enhance/springbootquickstart/courseapiEnhance/CourseApiEnhanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enhance.springbootquickstart.courseapiEnhance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiEnhanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
